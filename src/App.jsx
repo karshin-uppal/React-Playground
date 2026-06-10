d8dd585346182612d4364e36938c71d40a2ae58d
karshin-uppal <dev4@dipanetech.com>
2026-06-10 11:28:40 +0530

    React Practice

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Welcome from "./Welcome";
+import Btn from "./Button";
+
+
+
+function App() {
+  return (
+    <div>
+      <h1>Hello World!!</h1>
+      <Welcome />
+      <Btn />
+    </div>
+  );
+}
+
+export default App
